2fa505ace154d518cf7bc401a08f87f155cb6daa
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2020-02-04 14:24:09 +0100

    Integrated Material Kit React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Background from "./components/Background";
+import Favorite from "@material-ui/icons/Favorite";
 
 function App() {
   return (
     <div className="page-wrapper">
       <Background />
       <div className="page-content ">Test</div>
+      <Favorite />
     </div>
   );
 }
